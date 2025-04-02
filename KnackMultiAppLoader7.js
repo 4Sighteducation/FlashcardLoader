4b34af35cd873f5345fd699aa2a6e44e45ee9ac0
4Sighteducation <admin@vespa.academy>
2025-04-02 18:21:43 +0100

    Update and rename KnackMultiAppLoader6.js to KnackMultiAppLoader7.js

--- KnackMultiAppLoader7.js
+++ KnackMultiAppLoader7.js
@@ -15,7 +15,7 @@
         const APP_DEFINITIONS = {
             'flashcards': {
                 mapping: { 'scene_1206': 'view_3005' }, // Your Flashcards IDs
-                scriptUrl: 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/Integrations/Flashcards1b.js', // Corrected Flashcards Script
+                scriptUrl: 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/Flashcards1b.js', // Corrected Flashcards Script
                 configBuilder: function(sharedConfig, sceneId, viewId) { /* ... */ }
             },
             'studyplanner': {
