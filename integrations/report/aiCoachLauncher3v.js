3284d9d5513137470637819466d34de3165f56c3
4Sighteducation <admin@vespa.academy>
2025-05-30 08:56:37 +0100

    Update and rename aiCoachLauncher3u.js to aiCoachLauncher3v.js

--- integrations/report/aiCoachLauncher3v.js
+++ integrations/report/aiCoachLauncher3v.js
@@ -242,7 +242,7 @@ if (window.aiCoachLauncherInitialized) {
         link.id = styleId;
         link.rel = 'stylesheet';
         link.type = 'text/css';
-        link.href = 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/report/aiCoachLauncher1h.css';
+        link.href = 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/report/aiCoachLauncher1i.css';
         // .css';
         
         // Add dynamic CSS for config-specific IDs
