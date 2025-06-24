abb4e2042d2e34bdf27ab6dd60365d7a7aecbbc8
4Sighteducation <admin@vespa.academy>
2025-06-24 13:45:41 +0100

    Update and rename staffHomepage4t.js to staffHomepage4u.js

--- integrations/landingPage/staffHomepage4u.js
+++ integrations/landingPage/staffHomepage4u.js
@@ -4582,7 +4582,7 @@ document.head.appendChild(fontAwesomeLink);
 
 // Load Student Emulator Script
 const studentEmulatorScript = document.createElement('script');
-studentEmulatorScript.src = 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/landingPage/studentEmulator1n.js';
+studentEmulatorScript.src = 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/landingPage/studentEmulator1o.js';
 studentEmulatorScript.onload = () => {
   console.log('[Staff Homepage] Student Emulator script loaded successfully');
 };
