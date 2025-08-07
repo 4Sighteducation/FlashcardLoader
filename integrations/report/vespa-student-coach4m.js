aaa818c20b10ec6db4f1a86e65d77b336a95f081
4Sighteducation <admin@vespa.academy>
2025-08-07 14:23:28 +0100

    Update and rename vespa-student-coach4l.js to vespa-student-coach4m.js

--- integrations/report/vespa-student-coach4m.js
+++ integrations/report/vespa-student-coach4m.js
@@ -428,8 +428,8 @@ if (window.studentCoachLauncherInitialized) {
         
         // URLs for both themes
         const themeUrls = {
-            'cyberpunk': 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/report/cyberpunk2l.css',
-            'original': 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/report/original2l.css'
+            'cyberpunk': 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/report/cyberpunk2m.css',
+            'original': 'https://cdn.jsdelivr.net/gh/4Sighteducation/FlashcardLoader@main/integrations/report/original2m.css'
         };
         
         const newHref = themeUrls[theme] || themeUrls['cyberpunk'];
